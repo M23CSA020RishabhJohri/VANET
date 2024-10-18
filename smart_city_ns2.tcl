26db0e9d77b0b4a6b2d61fe0b73bef7dde49944e
M23CSA020RishabhJohri <164892321+M23CSA020RishabhJohri@users.noreply.github.com>
2024-10-18 17:44:15 +0530

    Update smart_city_ns2.tcl

--- smart_city_ns2.tcl
+++ smart_city_ns2.tcl
@@ -1,28 +1,3 @@
-# Define the simulation environment
-set ns [new Simulator]
-set tracefile [open out.tr w]
-$ns trace-all $tracefile
-
-set nf [open out.nam w]
-$ns namtrace-all $nf
-
-# Define a topology
-set num_nodes 10   ;# Total number of vehicles
-set rsu_nodes 2    ;# Number of RSUs
-
-# Create Nodes
-for {set i 0} {$i < $num_nodes} {incr i} {
-    set node($i) [$ns node]
-}
-
-for {set i 0} {$i < $rsu_nodes} {incr i} {
-    set rsu($i) [$ns node]
-}
-
-# Define TCP as the transmission model
-Agent/TCP set packetSize_ 1000
-Agent/TCP set window_ 32
-
 # Create TCP/FTP connections between vehicles and RSUs (V2R communication)
 set tcp [new Agent/TCP]
 $ns attach-agent $node(0) $tcp
@@ -30,10 +5,11 @@ set sink [new Agent/TCPSink]
 $ns attach-agent $rsu(0) $sink
 $ns connect $tcp $sink
 
+# Create Application/FTP for V2R communication
 set ftp [new Application/FTP]
 $ftp attach-agent $tcp
 $ftp set type_ FTP
-$ftp start 1.0
+$ns at 1.0 "$ftp start"  ;# Schedule the FTP application to start at time 1.0
 
 # Create vehicle-to-vehicle communication (V2V)
 set tcp2 [new Agent/TCP]
@@ -42,40 +18,8 @@ set sink2 [new Agent/TCPSink]
 $ns attach-agent $node(2) $sink2
 $ns connect $tcp2 $sink2
 
+# Create Application/FTP for V2V communication
 set ftp2 [new Application/FTP]
 $ftp2 attach-agent $tcp2
 $ftp2 set type_ FTP
-$ftp2 start 2.0
-
-# Define mobility
-$node(0) set X_ 10.0
-$node(0) set Y_ 10.0
-$node(1) set X_ 50.0
-$node(1) set Y_ 50.0
-$node(2) set X_ 100.0
-$node(2) set Y_ 100.0
-
-$rsu(0) set X_ 200.0
-$rsu(0) set Y_ 200.0
-
-$rsu(1) set X_ 300.0
-$rsu(1) set Y_ 300.0
-
-# Use AODV routing protocol for both V2V and V2R communication
-$ns node-config -adhocRouting AODV
-
-# Simulation parameters
-$ns at 50.0 "finish"
-
-# Finish function
-proc finish {} {
-    global ns tracefile nf
-    $ns flush-trace
-    close $tracefile
-    close $nf
-    exec nam out.nam &
-    exit 0
-}
-
-# Start the simulation
-$ns run
+$ns at 2.0 "$ftp2 start"  ;# Schedule the second FTP application to start at time 2.0
